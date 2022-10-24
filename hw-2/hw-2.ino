b1e94cc8c991556debf26c1eae48ae0469087b5d
Nicula Ionut <niculaionut@tutanota.com>
2022-10-24 21:47:59 +0300

    Add HW2 draft, remove -Wpedantic

---
+++ hw-2/hw-2.ino
@@ -0,0 +1,110 @@
+#include <Arduino.h>
+
+/* Enums */
+enum {
+    L_PED_RED = 0,
+    L_PED_GREEN,
+    L_CAR_RED,
+    L_CAR_YELLOW,
+    L_CAR_GREEN,
+    NUM_LEDS,
+};
+
+enum {
+    S_RED_LIGHT = 0,
+    S_RED_LIGHT_ENDING,
+    S_YELLOW_LIGHT,
+    S_GREEN_LIGHT,
+    S_GREEN_LIGHT_ENDING,
+    NUM_SEM_STATES,
+};
+
+/* Structs */
+struct Led {
+    int outputPin;
+};
+
+/* Constants */
+static constexpr Led LEDS[NUM_LEDS] = {
+    { 2 },
+    { 3 },
+    { 4 },
+    { 5 },
+    { 6 },
+};
+
+static constexpr unsigned DURATIONS[NUM_SEM_STATES] = {
+    [S_RED_LIGHT] = (unsigned)-1,
+    [S_RED_LIGHT_ENDING] = 10,
+    [S_YELLOW_LIGHT] = 3,
+    [S_GREEN_LIGHT] = 10,
+    [S_GREEN_LIGHT_ENDING] = 5,
+};
+
+static constexpr unsigned LED_STATES[NUM_SEM_STATES] = {
+    0b10001,
+    0b10001,
+    0b01001,
+    0b00110,
+    0b00110,
+};
+
+/* Semaphore state */
+static int currentSemState;
+static unsigned long prevTs;
+
+/* Functions */
+static void updateLeds(const unsigned ledStates)
+{
+    static constexpr unsigned mask = 1;
+    for (unsigned i = 0; i < NUM_LEDS; ++i) {
+        const unsigned value = ledStates & (mask << i);
+        digitalWrite(LEDS[i].outputPin, bool(value));
+    }
+}
+
+void setup()
+{
+    static constexpr unsigned BAUD = 9600;
+    Serial.begin(BAUD);
+
+    currentSemState = S_RED_LIGHT;
+    prevTs = millis();
+}
+
+void loop()
+{
+    const int oldSemState = currentSemState;
+
+    switch (currentSemState) {
+    case S_RED_LIGHT: /* Fallthrough */
+        if (true) {
+            prevTs = millis();
+            currentSemState = S_RED_LIGHT_ENDING;
+        }
+    case S_RED_LIGHT_ENDING: /* Fallthrough */
+    case S_YELLOW_LIGHT:
+        /* No further action necessary */
+        break;
+    case S_GREEN_LIGHT:
+        break;
+    case S_GREEN_LIGHT_ENDING:
+        break;
+    }
+
+    const auto currentTs = millis();
+    if (currentTs - prevTs > DURATIONS[currentSemState]) {
+        prevTs = currentTs;
+        currentSemState = (currentSemState + 1) % NUM_SEM_STATES;
+    }
+
+    if (currentSemState != oldSemState)
+        updateLeds(LED_STATES[currentSemState]);
+}
+
+int main()
+{
+    setup();
+    for (;;)
+        loop();
+}
