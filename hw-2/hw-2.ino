56a5fd7677b3ccb088676c110f9e4c427a1652f6
Nicula Ionut <niculaionut@tutanota.com>
2022-10-25 23:24:54 +0300

    Naming

--- hw-2/hw-2.ino
+++ hw-2/hw-2.ino
@@ -10,103 +10,133 @@ enum Led {
     NumLeds,
 };
 
-enum SemState {
+enum CrossState {
     PedRedLight = 0,
     PedRedLightEnding,
     CarYellowLight,
     PedGreenLight,
     PedGreenLightEnding,
-    NumSemStates,
+    NumCrossStates,
 };
 
 /* Structs */
 struct LedController {
-    int outputPin;
+    uint8_t outputPin;
 };
 
 /* Constants */
-static constexpr LedController LED_CONTROLLERS[NumLeds] = {
-    [Led::PedRed] = { 2 },
-    [Led::PedGreen] = { 3 },
-    [Led::CarRed] = { 4 },
-    [Led::CarYellow] = { 5 },
-    [Led::CarGreen] = { 6 },
-};
-
-static constexpr unsigned DURATIONS[NumSemStates] = {
-    [SemState::PedRedLight] = (unsigned)-1,
-    [SemState::PedRedLightEnding] = 10,
-    [SemState::CarYellowLight] = 3,
-    [SemState::PedGreenLight] = 10,
-    [SemState::PedGreenLightEnding] = 5,
-};
-
-static constexpr unsigned LED_STATES[NumSemStates] = {
-    [SemState::PedRedLight] = 0b10001,
-    [SemState::PedRedLightEnding] = 0b10001,
-    [SemState::CarYellowLight] = 0b01001,
-    [SemState::PedGreenLight] = 0b00110,
-    [SemState::PedGreenLightEnding] = 0b00110,
-};
-
 static constexpr unsigned long GREEN_LIGHT_BLINK_INTERVAL = 100;
+static constexpr uint8_t BUTTON_PIN = 2;
+static constexpr uint8_t BUZZER_PIN = 3; /* PWM pin */
+static constexpr unsigned NOTE_FS5 = 784;
 
-/* Semaphore state */
-static int currentSemState;
+static constexpr LedController LED_CONTROLLERS[NumLeds] = {
+    [Led::PedRed] = { 4 },
+    [Led::PedGreen] = { 5 },
+    [Led::CarRed] = { 6 },
+    [Led::CarYellow] = { 7 },
+    [Led::CarGreen] = { 8 },
+};
+
+static constexpr unsigned DURATIONS[NumCrossStates] = {
+    [CrossState::PedRedLight] = (unsigned)-1,
+    [CrossState::PedRedLightEnding] = 10,
+    [CrossState::CarYellowLight] = 3,
+    [CrossState::PedGreenLight] = 10,
+    [CrossState::PedGreenLightEnding] = 5,
+};
+
+static constexpr uint8_t LED_STATES[NumCrossStates] = {
+    [CrossState::PedRedLight] = 0b10001,
+    [CrossState::PedRedLightEnding] = 0b10001,
+    [CrossState::CarYellowLight] = 0b01001,
+    [CrossState::PedGreenLight] = 0b00110,
+    [CrossState::PedGreenLightEnding] = 0b00110,
+};
+
+/* Crosswalk state */
+static uint8_t currentCrossState;
 static unsigned long prevTs;
 
+/* Macros */
+#define BUTTON_PRESSED (digitalRead(BUTTON_PIN) == HIGH)
+
 /* Functions */
-static void updateLeds(const unsigned ledStates)
+static void updateLeds(const uint8_t ledStates)
 {
-    static constexpr unsigned mask = 1;
-    for (unsigned i = 0; i < NumLeds; ++i) {
-        const unsigned value = ledStates & (mask << i);
+    static constexpr uint8_t mask = 1;
+    for (uint8_t i = 0; i < NumLeds; ++i) {
+        const uint8_t value = ledStates & (mask << i);
         digitalWrite(LED_CONTROLLERS[i].outputPin, bool(value));
     }
 }
 
 void setup()
 {
-    static constexpr unsigned BAUD = 9600;
-    Serial.begin(BAUD);
-
     /* Init semaphore state */
-    currentSemState = SemState::PedRedLight;
+    currentCrossState = CrossState::PedRedLight;
     prevTs = millis();
 
     /* Init output LED pins */
     for (auto& lc : LED_CONTROLLERS)
         pinMode(lc.outputPin, OUTPUT);
+
+    /* Init button pin */
+    pinMode(BUTTON_PIN, INPUT);
+
+    /* Init buzzer pin */
+    pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop()
 {
-    const int oldSemState = currentSemState;
+    const uint8_t oldSemState = currentCrossState;
     const auto currentTs = millis();
 
-    switch (currentSemState) {
-    case SemState::PedRedLight: /* Fallthrough */
-        if (true) {
+    /* Handle the current state */
+    switch (currentCrossState) {
+    case CrossState::PedRedLight: /* Fallthrough */
+        if (BUTTON_PRESSED) {
             prevTs = currentTs;
-            currentSemState = SemState::PedRedLightEnding;
+            currentCrossState = CrossState::PedRedLightEnding;
         }
-    case SemState::PedRedLightEnding: /* Fallthrough */
-    case SemState::CarYellowLight:
+    case CrossState::PedRedLightEnding: /* Fallthrough */
+    case CrossState::CarYellowLight: /* Fallthrough */
+    case CrossState::PedGreenLight:
         /* No further action necessary */
         break;
-    case SemState::PedGreenLight:
+    case CrossState::PedGreenLightEnding:
+        if ((currentTs / GREEN_LIGHT_BLINK_INTERVAL) % 2) {
+            tone(BUZZER_PIN, NOTE_FS5);
+            digitalWrite(LED_CONTROLLERS[Led::PedGreen].outputPin, HIGH);
+        } else {
+            noTone(BUZZER_PIN);
+            digitalWrite(LED_CONTROLLERS[Led::PedGreen].outputPin, LOW);
+        }
         break;
-    case SemState::PedGreenLightEnding:
-        break;
+    default:
+        __builtin_unreachable();
     }
 
-    if (currentTs - prevTs > DURATIONS[currentSemState]) {
+    if (currentTs - prevTs > DURATIONS[currentCrossState]) {
         prevTs = currentTs;
-        currentSemState = (currentSemState + 1) % NumSemStates;
+        currentCrossState = (currentCrossState + 1) % NumCrossStates;
     }
 
-    if (currentSemState != oldSemState)
-        updateLeds(LED_STATES[currentSemState]);
+    if (currentCrossState != oldSemState) {
+        /* Handle state transition */
+        updateLeds(LED_STATES[currentCrossState]);
+
+        switch (currentCrossState) {
+        case CrossState::PedGreenLight:
+            tone(BUZZER_PIN, NOTE_FS5);
+            break;
+        case CrossState::PedGreenLightEnding:
+            noTone(BUZZER_PIN);
+        default:
+            break;
+        }
+    }
 }
 
 int main()
