3c7306122c7e930ddbaf21c555eb367bf1d277c7
Nicula Ionut <niculaionut@tutanota.com>
2022-11-01 14:25:39 +0200

    Add clamp and run clang-format on utils.h

--- common/utils.h
+++ common/utils.h
@@ -1,24 +1,32 @@
 /*
  *  Minimal implementations of some STL components:
  *      std::pair,
- *      std::for_each
+ *      std::for_each,
+ *      std::clamp
  */
 
 #pragma once
 
 namespace Tiny {
 /* <utility> */
-template <typename T, typename U>
-struct Pair {
+template <typename T, typename U> struct Pair {
     T first;
     U second;
 };
 
 /* <algorithm> */
-template <typename Container, typename Callable>
-void forEach(Container& cont, Callable call)
+template <typename Container, typename Callable> void forEach(Container& cont, Callable call)
 {
     for (auto& el : cont)
         call(el);
+}
+
+template <typename T> const T& clamp(const T& x, const T& low, const T& high)
+{
+    if (x < low)
+        return low;
+    if (x > high)
+        return high;
+    return x;
 }
 }
